9ddf27907024fd4536ba8d9dfd814ed0595792b4
Felipe Botero <frpbotero@gmail.com>
2023-02-15 12:47:19 -0400

    Estilização Modal Cautela de entrega

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,16 @@ import { HeaderADM } from "./components/HeaderADM"
 import { HeaderDelivery } from "./components/HeaderDelivery"
 import { Login } from "./pages/Login"
 import { GestADM } from "./pages/GestADM"
-
+import "./App.css"
 import Modal from 'react-modal';
 
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
 Modal.setAppElement('#root');
 function App() {
- 
   return (
     <div className="App">
       <HeaderDelivery />
       <div className="content">
-      <GestADM />
-
+        <GestADM />
       </div>
     </div>
   )
