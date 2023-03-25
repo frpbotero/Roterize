fa0a433a696f2371dd0602df511230ff17299e2c
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-24 21:33:59 -0400

    Update Main
    Foi alterado a forma de importação das rotas para melhoria na organização.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
+import { Login } from "./pages/Login";
+import { GestADM } from "./pages/GestADM";
+import { Clientes } from "./pages/Clientes";
+import { Produtos } from "./pages/Produtos";
+import { Entregas } from "./pages/Entregas";
+import { Delivery } from "./pages/Delivery";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Login /> },
+      { path: "/gestadm", element: <GestADM /> },
+      { path: "/client", element: <Clientes /> },
+      { path: "/product", element: <Produtos /> },
+      { path: "/route", element: <Entregas /> },
+      { path: "/delivery", element: <Delivery /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
