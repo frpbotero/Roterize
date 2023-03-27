8a464ea27e15ba9c21c4f76b5a4332f4d51ce9a0
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-26 22:06:09 -0400

    update styles header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,14 @@
-import logo  from "../assets/Router2.svg"
-import "./Header.css"
-import { Link } from "react-router-dom"
+import logo from "../assets/Router2.svg";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
-export function Header(){
-
- 
-
-    return(
-        <div className="containerHeader">
-        <img src={logo} alt="" />
-        <Link to="/login"><button >Login</button></Link>
-        </div>
-    )
+export function Header() {
+  return (
+    <div className="containerHeader">
+      <img src={logo} alt="" />
+      <Link to="/login">
+        <button className="buttonLogin">Login</button>
+      </Link>
+    </div>
+  );
 }
