259e72d098f29a188eb6292024626b917040f87b
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-06 11:01:35 -0400

    Try erro vercel pdfMake

--- frontend/src/fonts/vfs_fonts.d.ts
+++
@@ -1,1 +0,0 @@
-export const pdfFonts: string = { "Roboto-Regular.ttf": "{...}" };
---
+++ frontend/src/pages/ReportID/fonts/vfs_fonts.d.ts
@@ -0,0 +1,7 @@
+const pdfFonts = { "Roboto-Regular.ttf": "{...}" };
+
+export default pdfFonts;
+
+// export namespace pdfMake {
+//     const vfs: { [file: string]: string };
+// }
