49ac46a462b2d1df3017a227a9644ce8e1a37e6a
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-08 19:22:43 -0400

    update Header include datePicker for select date.

--- Mobile/src/components/Header/index.js
+++ Mobile/src/components/Header/index.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   StyleSheet,
   Text,
   StatusBar,
   Image,
-  Button,
   TouchableOpacity,
+  Modal,
 } from "react-native";
+import DatePicker from "react-native-modern-datepicker";
 
 const statusBarHeight = StatusBar.currentHeight
   ? StatusBar.currentHeight + 22
@@ -18,12 +19,37 @@ function onPress() {
 }
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  function handleModal() {
+    setOpen(!open);
+  }
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.tinyLogo}
         source={require("../../../assets/router.png")}
       />
+      <TouchableOpacity onPress={handleModal}>
+        <Text>Data</Text>
+      </TouchableOpacity>
+
+      <Modal animationType="slide" transparent={true} visible={open}>
+        <View style={styles.viewCentered}>
+          <View style={styles.modalView}>
+            <TouchableOpacity onPress={handleModal}>
+              <Text>X</Text>
+            </TouchableOpacity>
+            <DatePicker
+              format="DD/MM/YYYY"
+              style={styles.dateComponent}
+              date={this.state.data}
+            />
+          </View>
+        </View>
+      </Modal>
+
       <TouchableOpacity style={styles.appButtonContainer}>
         <Text style={styles.appButtonText}>Login</Text>
       </TouchableOpacity>
@@ -35,7 +61,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#FFF",
     paddingTop: statusBarHeight,
-    paddingHorizontal: 3,
+    paddingHorizontal: 10,
+    alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-between",
   },
@@ -48,9 +75,9 @@ const styles = StyleSheet.create({
     height: 58,
   },
   appButtonContainer: {
-    elevation: 8,
+    elevation: 5,
     backgroundColor: "#fff",
-    borderColor: "rgb(221, 87, 87)",
+    borderColor: "1px solid rgb(221, 87, 87)",
     borderRadius: 10,
     paddingVertical: 10,
     paddingHorizontal: 12,
@@ -61,5 +88,30 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     alignSelf: "center",
     textTransform: "uppercase",
+  },
+  dateComponent: {
+    width: 250,
+  },
+  viewCentered: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    width: "90%",
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
