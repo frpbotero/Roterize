0c81dd019719b2c127b1f3d1f283a0bf2094c9ee
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-22 13:07:41 -0400

    change extensions TS and create interface IUser

---
+++ backend/src/model/permissions.md
@@ -0,0 +1,12 @@
+Type permission{
+
+create
+update
+delete
+get
+}
+type resource{
+ADM = All use
+Colab = Create, update and get
+Delivery = get and update
+}
---
+++ backend/src/model/user.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import moment from "moment/moment";
+
+interface IUser {
+  name: string;
+  cpf: string;
+  permission: string;
+  email: string;
+  password: string;
+  createdAt: string | Date;
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, require: true },
+  cpf: { type: String, require: true },
+  permission: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  createdAt: {
+    type: String,
+    default: moment(new Date()).format("DD/MM/YYYY, HH:mm:ss"),
+  },
+});
+export const User = mongoose.model<IUser>("User", userSchema);
