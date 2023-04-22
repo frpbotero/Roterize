941a8dbacc2de0d2197b4e49acf751420576f9b6
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-22 16:06:02 -0400

    change extension product.service for product.service.ts

---
+++ backend/src/service/products.service.ts
@@ -0,0 +1,26 @@
+import { productRepository } from "../repository/product.repository.js";
+import { IProduct } from "../model/products.model.js";
+
+function getAll() {
+  return productRepository.getAll();
+}
+function getByID(id: string) {
+  return productRepository.getByID(id);
+}
+function create(body: IProduct) {
+  return productRepository.create(body);
+}
+function updateUser(id: string, body: Partial<IProduct>) {
+  return productRepository.update(id, body);
+}
+function deleteUser(id: string) {
+  return productRepository.delete(id);
+}
+
+export default {
+  getAll,
+  getByID,
+  create,
+  updateUser,
+  deleteUser,
+};
