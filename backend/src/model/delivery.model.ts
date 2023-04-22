90518d8af348ecf084cb5946cd428c12a0553f81
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-22 12:49:22 -0400

    change extension TS and create Interface for delivery

---
+++ backend/src/model/delivery.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import moment from "moment/moment";
+
+interface IProducts {
+  name: string;
+  qtd: number;
+}
+
+interface IDelivery {
+  client: Object;
+  deliveryList: Array<IProducts>;
+  descriptionDelivery: string;
+  signature: string;
+  status: string;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+}
+
+const deliverySchema = new Schema<IDelivery>({
+  client: { type: Object, require: true },
+  deliveryList: { type: [Object], require: true },
+  descriptionDelivery: { type: String, require: true },
+  signature: { type: String },
+  status: { type: String, require: true },
+  createdAt: { type: String, require: true },
+  updatedAt: { type: String, require: true },
+});
+export const Delivery = mongoose.model("delivery", deliverySchema);
