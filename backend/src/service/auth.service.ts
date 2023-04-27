9374237abbad7338d03cbaaa5a81e47e0d24b838
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-27 16:08:29 -0400

    Create auth.service.ts

---
+++ backend/src/service/auth.service.ts
@@ -0,0 +1,26 @@
+import { userRespository } from "../repository/user.repository";
+import { IUser } from "../model/user.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const secretKey = process.env.SECRET_KEY || "";
+
+async function auth(email: string, password: string) {
+  const user = await userRespository.getByEmail(email);
+
+  if (!user) throw new Error("Usuário não encontrado!");
+
+  try {
+    const confirmPass = await bcrypt.compare(password, user.password);
+
+    if (!confirmPass) {
+      throw new Error("Falha na autenticação!");
+    }
+    return jwt.sign({ id: user.id }, secretKey, { expiresIn: "1h" });
+  } catch {
+    throw new Error("Falha na autenticação!");
+  }
+}
